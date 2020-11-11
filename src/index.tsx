d978ebc412ccafbe7d0483f0479f44ec11d27ff3
Isaac Adams <isaac.d.adams@gmail.com>
2020-11-11 00:20:23 -0500

    adding eslint and fetching data

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,5 @@
-console.log("hello world");
+let race: 'president' | 'senate' | 'house' = 'president';
+let state = 'pennsylvania';
+
+let url = `https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/race-page/${state}/${race}.json`;
+fetch(url).then(r => r.json()).then(console.log);
