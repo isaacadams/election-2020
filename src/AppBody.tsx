d9ff4248dee24466dea4d234076e9c4e4d7dcfae
Isaac Adams <isaac.d.adams@gmail.com>
2020-11-16 19:02:29 -0500

    abstracting out the form view from the main body

--- src/AppBody.tsx
+++ src/AppBody.tsx
@@ -1,6 +1,33 @@
 import * as React from 'react';
-import {SelectElectionData} from './SelectData';
+import {Col, Container, Row} from 'react-bootstrap';
+import {UseChart} from './UseChart';
+import {VoteChangesChart} from './VoteChangesChart';
+import {UseElectionData} from './UseElectionData';
+import {SelectElectionDataForm} from './SelectElectionDataForm';
 
 export function AppBody(props): JSX.Element {
-  return <SelectElectionData />;
+  let {state, race, formView} = SelectElectionDataForm({});
+  let data = UseElectionData({state, race});
+
+  return (
+    <Container>
+      <Row>
+        <Col>{formView}</Col>
+      </Row>
+      <Row className="mt-3">
+        {data && (
+          <Col>
+            <UseChart data={data} />
+          </Col>
+        )}
+      </Row>
+      <Row className="mt-3">
+        {data && (
+          <Col>
+            <VoteChangesChart data={data} />
+          </Col>
+        )}
+      </Row>
+    </Container>
+  );
 }
--- src/SelectElectionDataForm.tsx
+++ src/SelectElectionDataForm.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import {Col, Container, Row} from 'react-bootstrap';
+import {UseSelect} from './UseSelect';
 import rawStatesData from './states';
 import {IStateModel} from './IStateModel';
-import {UseSelect} from './UseSelect';
-import {UseChart} from './UseChart';
-import {VoteChangesChart} from './VoteChangesChart';
-import {UseElectionData} from './UseElectionData';
 
 let states: IStateModel[] = rawStatesData.map((s) => ({
   name: s.state,
@@ -13,7 +10,13 @@ let states: IStateModel[] = rawStatesData.map((s) => ({
   abbrev: s.abbrev,
 }));
 
-export function SelectElectionData(props): JSX.Element {
+interface IReturn {
+  state: string | number | readonly string[];
+  race: string | number | readonly string[];
+  formView: JSX.Element;
+}
+
+export function SelectElectionDataForm(props): IReturn {
   let race = UseSelect({
     options: ['president', 'senate', 'house'],
     name: 'race',
@@ -32,28 +35,20 @@ export function SelectElectionData(props): JSX.Element {
     className: 'form-control',
   });
 
-  let data = UseElectionData({state: state.selected, race: race.selected});
-
-  return (
+  let formView = (
     <Container>
       <Row>
-        <Col>{race.view}</Col>
         <Col>{state.view}</Col>
       </Row>
-      <Row className="mt-3">
-        {data && (
-          <Col>
-            <UseChart data={data} />
-          </Col>
-        )}
-      </Row>
-      <Row className="mt-3">
-        {data && (
-          <Col>
-            <VoteChangesChart data={data} />
-          </Col>
-        )}
+      <Row>
+        <Col>{race.view}</Col>
       </Row>
     </Container>
   );
+
+  return {
+    state: state.selected,
+    race: race.selected,
+    formView,
+  };
 }
